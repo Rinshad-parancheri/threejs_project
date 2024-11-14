9236eb69614fe7e7c0ad04c9fedd44c25bee8f44
Rinshad-parancheri <rinshadp014@gmail.com>
2024-11-14 07:46:26 +0530

    add review section

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
-import MyPage from "./section/MyPage"
+import About from "./section/About"
+import Clients from "./section/Clients"
+import Hero from "./section/Hero"
+import Navbar from "./section/Navbar"
+import Project from "./section/Projects"
 const App = () => {
   return (
-    <MyPage />
+    <main className="max-w-[1400px] mx-auto relative">
+      <Navbar />
+      <Hero />
+      <About />
+      <Project />
+      <Clients />
+    </main>
+
   )
 }
 
